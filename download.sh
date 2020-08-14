e9b731e03297527c4cdc46f46b89a1e06e142970
Magd Kudama <magd.kudama@city.uk.com>
2020-08-14 03:20:01 +0200

    Initial commit

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/tsenart/vegeta/releases/download/cli%2Fv12.5.1/vegeta-12.5.1-linux-amd64.tar.gz
+tar xfz vegeta-12.5.1-linux-amd64.tar.gz
+mv vegeta /usr/local/bin
+
