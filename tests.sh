d2e86f92c62cf6a341aa5ed9e82b2853f4be27fb
Magd Kudama <magd.kudama@city.uk.com>
2020-08-14 03:15:39 +0200

    Initial commit

---
+++ tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ulimit -S -n 4096
+sysctl -w fs.inotify.max_user_watches=100000
+
+python3 generator.py
+
+for rate in 100 150 200 300; do
+  vegeta attack -rate="$rate" -duration=300s -targets=targets.txt > "results-$rate.bin"
+done
+
+vegeta report -inputs="$(echo results-*.bin | tr ' ' ',')"
